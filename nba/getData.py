a2661e72bef0952a3658e764ceebe27e1c9fa330
Brent Zhao <brentzhao@berkele.edu>
2018-11-17 19:06:45 -0800

    data extracted

--- nba/getData.py
+++ nba/getData.py
@@ -2,73 +2,92 @@ class Team:
 
     def __init__(self, team_name):
         self.name = team_name
-        self.players = []
+        self.players = {}
+
+    def add_player(self, entry):
+        if entry[1] in self.players:
+            self.players[entry[1]].add_stats(entry)
+        else:
+            self.players[entry[1]] = Player(entry[1])
+            self.players[entry[1]].add_stats(entry)
 
 
 class Player:
 
     def __init__(self, gamertag):
         self.name = gamertag
-        self.tip_off = Stats()
-        self.regular = Stats()
-        self.play_off = Stats()
-        self.mid_session1 = Stats()
-        self.mid_session2 = Stats()
+        self.tip_off = None
+        self.regular = None
+        self.play_off = None
+        self.mid_session1 = None
+        self.mid_session2 = None
         self.cur_pos = ""
+
+    def add_stats(self, entry):
+        if entry[2] == 'Tip Off':
+            self.tip_off = Stats(entry)
+        elif entry[2] == 'Regular Season':
+            self.regular = Stats(entry)
+        elif entry[2] == 'Playoffs':
+            self.play_off = Stats(entry)
+        elif entry[2] == 'Mid-Season Tournament 1':
+            self.mid_session1 = Stats(entry)
+        elif entry[2] == 'Mid-Season Tournament 2':
+            self.mid_session2 = Stats(entry)
 
 
 class Stats:
 
-    def __init__(self):
+    def __init__(self, entry):
         # Season Total
-        self.gp = 0                 # game played
-        self.pts = 0                # points
-        self.ast = 0                # assists
-        self.to = 0                 # turnovers
-        self.ast_to_ratio = 0       # assists / turnovers
-        self.stl = 0                # steals
-        self.blk = 0                # blocks
-        self.reb = 0                # rebounds
-        self.oreb = 0               # offensive rebounds
-        self.dreb = 0               # defensive rebounds
-        self.fga = 0                # field goal attempted
-        self.fgm = 0                # field goal made
-        self.fg_percent = 0         # field goal percentage
-        self.e_fg_percent = 0       # effective field goal percentage
-        self.two_fga = 0            # two-point field goal attempted
-        self.two_fgm = 0            # two-point field goal made
-        self.two_fg_percent = 0     # two-point field goal percentage
-        self.three_fga = 0          # three-point field goal attempted
-        self.three_fgm = 0          # three-point field goal made
-        self.three_fg_percent = 0   # three-point field goal percentage
-        self.fta = 0                # free throw attempted
-        self.ftm = 0                # free throw made
-        self.ft_percent = 0         # free throw percent
-        self.pf_tkn = 0             # personal fouls taken
-        self.pf = 0                 # personal fouls
+        self.gp = entry[3]                 # game played
+        self.pts = entry[4]                # points
+        self.ast = entry[5]                # assists
+        self.to = entry[6]                 # turnovers
+        self.ast_to_ratio = entry[7]       # assists / turnovers
+        self.stl = entry[8]                # steals
+        self.blk = entry[9]                # blocks
+        self.reb = entry[10]               # rebounds
+        self.oreb = entry[11]              # offensive rebounds
+        self.dreb = entry[12]              # defensive rebounds
+        self.fga = entry[13]               # field goal attempted
+        self.fgm = entry[14]               # field goal made
+        self.fg_percent = entry[15]        # field goal percentage
+        self.e_fg_percent = entry[16]      # effective field goal percentage
+        self.two_fga = entry[17]           # two-point field goal attempted
+        self.two_fgm = entry[18]           # two-point field goal made
+        self.two_fg_percent = entry[19]    # two-point field goal percentage
+        self.three_fga = entry[20]         # three-point field goal attempted
+        self.three_fgm = entry[21]         # three-point field goal made
+        self.three_fg_percent = entry[22]  # three-point field goal percentage
+        self.fta = entry[23]               # free throw attempted
+        self.ftm = entry[24]               # free throw made
+        self.ft_percent = entry[25]        # free throw percent
+        self.pf_tkn = entry[26]            # personal fouls taken
+        self.pf = entry[27]                # personal fouls
         # Per Game Stats
-        self.ppg = 0                    # points per game
-        self.apg = 0                    # assists per game
-        self.to_pg = 0                  # turn overs per game
-        self.ast_to_ratio_pg = 0        # assists / turnovers per game
-        self.stl_pg = 0                 # steals per game
-        self.blk_pg = 0                 # blocks per game
-        self.rpg = 0                    # rebounds per game
-        self.oreb_pg = 0                # offensive rebounds per game
-        self.dreb_pg = 0                # defensive rebounds per game
-        self.fga_pg = 0                 # field goal attempted per game
-        self.fgm_pg = 0                 # field goal made per game
-        self.fg_percent_pg = 0          # field goal percentage per game
-        self.e_fg_percent_pg = 0        # effective field goal percentage per game
-        self.two_fga_pg = 0             # two-point field goal attempted per game
-        self.two_fgm_pg = 0             # two-point field goal made per game
-        self.two_fg_percent_pg = 0      # two-point field goal percentage per game
-        self.three_fga_pg = 0           # three-point field goal attempted
-        self.three_fgm_pg = 0           # three-point field goal made
-        self.three_fg_percent_pg = 0    # three-point field goal percentage
-        self.fta_pg = 0                 # free throw attempted per game
-        self.ftm_pg = 0                 # free throw made per game
-        self.ft_percent_pg = 0          # free throw percentage
-        self.pf_tkn_pg = 0              # personal fouls taken per game
-        self.pf_pg = 0                  # personal fouls per game
+        self.ppg = entry[29]                    # points per game
+        self.apg = entry[30]                    # assists per game
+        self.to_pg = entry[31]                  # turn overs per game
+        self.ast_to_ratio_pg = entry[32]        # assists / turnovers per game
+        self.stl_pg = entry[33]                 # steals per game
+        self.blk_pg = entry[34]                 # blocks per game
+        self.rpg = entry[35]                    # rebounds per game
+        self.oreb_pg = entry[36]                # offensive rebounds per game
+        self.dreb_pg = entry[37]                # defensive rebounds per game
+        self.fga_pg = entry[38]                 # field goal attempted per game
+        self.fgm_pg = entry[39]                 # field goal made per game
+        self.fg_percent_pg = entry[40]          # field goal percentage per game
+        self.e_fg_percent_pg = entry[41]        # effective field goal percentage per game
+        self.two_fga_pg = entry[42]             # two-point field goal attempted per game
+        self.two_fgm_pg = entry[43]             # two-point field goal made per game
+        self.two_fg_percent_pg = entry[43]      # two-point field goal percentage per game
+        self.three_fga_pg = entry[44]           # three-point field goal attempted
+        self.three_fgm_pg = entry[45]           # three-point field goal made
+        self.three_fg_percent_pg = entry[46]    # three-point field goal percentage
+        self.fta_pg = entry[47]                 # free throw attempted per game
+        self.ftm_pg = entry[48]                 # free throw made per game
+        self.ft_percent_pg = entry[49]          # free throw percentage
+        self.pf_tkn_pg = entry[50]              # personal fouls taken per game
+        self.pf_pg = entry[51]                  # personal fouls per game
 
--- nba/main.py
+++ nba/main.py
@@ -5,32 +5,18 @@ file = pd.ExcelFile("NBA.xlsx")
 
 data = pd.read_excel(file, usecols="C:BC", skiprows=[0, 1, 2, 3, 4, 5])
 
-print(data.values)
 
-heatcheck_gaming = Team("Heatcheck Gaming")
-raptors_uprising_gc = Team("Raptors Uprising GC")
-celtics_crossover_gaming = Team("Celtics Crossover Gaming")
-grizz_gaming = Team("Grizz Gaming")
-mavs_gaming = Team("Mavs Gaming")
-bucks_gaming = Team("Bucks Gaming")
-wizards_district_gaming = Team("Wizards District Gaming")
-warriors_gaming_squad = Team("Warriors Gaming Squad")
-kings_guard_gaming = Team("Kings Guard Gaming")
-blazers_gaming = Team("Blazers Gaming")
-jazz_gaming = Team("Jazz Gaming")
-pacers_gaming = Team("Pacers Gaming")
-cavs_legion_gc = Team("Cavs Legion GC")
-knicks_gaming = Team("Knicks Gaming")
-seven_sixers_gc = Team("76ers GC")
-pistons_gt = Team("Pistons GT")
-magic_gaming = Team("Magic Gaming")
+def initialize_data(dataframe):
+    teams_dic = {}
+    data_list = dataframe.values
+    for entry in data_list:
+        if entry[0] in teams_dic:
+            teams_dic[entry[0]].add_player(entry)
+        else:
+            teams_dic[entry[0]] = Team(entry[0])
+            teams_dic[entry[0]].add_player(entry)
+    return teams_dic
 
 
-def contains(team_list, name):
-    for team in team_list:
-        if team.name == name:
-            return True
-    return False
-
-
-teams = []
+teams = initialize_data(data)
+print(teams["Heatcheck Gaming"].players["24KDropOff"].tip_off.pts)
